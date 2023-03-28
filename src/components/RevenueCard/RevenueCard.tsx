5746d398116c75631ea11e444ca1680364a62f31
tonwayer <tonwayer@gmail.com>
2023-03-27 22:05:14 -0400

    fix: ui with tailwindcss

--- src/components/RevenueCard/RevenueCard.tsx
+++ src/components/RevenueCard/RevenueCard.tsx
@@ -14,33 +14,44 @@ export const RevenueCard = (props: IRevenueCardProps) => {
   } = props;
 
   return (
-    <section aria-label="revenue-card">
-      <h2>Earnings</h2>
+    <section
+      aria-label="revenue-card"
+      className="border p-4 border-blue-300/80 rounded-md w-[320px]"
+    >
+      <h2>Revenue</h2>
       <div className="mt-8">
         <div>
           <p>Premium rooms</p>
-          <div>
-            Room:{" "}
-            <span data-testid="premium-occupancy">{premiumRoomOccupancy}</span>
-          </div>
-          <div>
-            Revenue:{" "}
-            <span data-testid="premium-revenue">
-              &euro;{premiumRoomRevenue}
-            </span>
+          <div className="flex justify-around text-center">
+            <div className="flex flex-col">
+              <img alt="premium rooms" className="w-10" src="/premium.png" />
+              <span data-testid="premium-occupancy">
+                {premiumRoomOccupancy}
+              </span>
+            </div>
+            <div>
+              <img alt="premium revenue" className="w-10" src="/revenue.png" />
+              <span data-testid="premium-revenue">
+                &euro;{premiumRoomRevenue}
+              </span>
+            </div>
           </div>
         </div>
         <div>
           <p>Economy rooms</p>
-          <div>
-            Rooms:{" "}
-            <span data-testid="economy-occupancy">{economyRoomOccupancy}</span>
-          </div>
-          <div>
-            Revenue:{" "}
-            <span data-testid="economy-revenue">
-              &euro;{economyRoomRevenue}
-            </span>
+          <div className="flex justify-around text-center">
+            <div className="flex flex-col">
+              <img alt="economy rooms" className="w-10" src="/economy.png" />
+              <span data-testid="economy-occupancy">
+                {economyRoomOccupancy}
+              </span>
+            </div>
+            <div>
+              <img alt="economy revenue" className="w-10" src="/revenue.png" />
+              <span data-testid="economy-revenue">
+                &euro;{economyRoomRevenue}
+              </span>
+            </div>
           </div>
         </div>
       </div>
