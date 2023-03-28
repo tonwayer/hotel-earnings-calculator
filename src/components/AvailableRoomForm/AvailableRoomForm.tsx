bb25350d0fd0279d84352db3cb7592b081b45355
tonwayer <tonwayer@gmail.com>
2023-03-27 22:10:51 -0400

    fix: AvailableRoomForm - Calculate Earnings => Calculate Revenue - Labels

--- src/components/AvailableRoomForm/AvailableRoomForm.tsx
+++ src/components/AvailableRoomForm/AvailableRoomForm.tsx
@@ -34,9 +34,9 @@ export const AvailableRoomForm = (props: IAvalableRoomFormProps) => {
           <div>
             <label
               htmlFor="numberOfPremiumRooms"
-              className="mb-2 text-sm font-medium text-gray-900 mr-1"
+              className="mb-2 text-sm font-medium text-gray-900 mr-2"
             >
-              Premium Rooms:{" "}
+              Premium Rooms
             </label>
             <input
               id="numberOfPremiumRooms"
@@ -56,9 +56,9 @@ export const AvailableRoomForm = (props: IAvalableRoomFormProps) => {
           <div>
             <label
               htmlFor="numberOfEconomyRooms"
-              className="mb-2 text-sm font-medium text-gray-900 mr-1"
+              className="mb-2 text-sm font-medium text-gray-900 mr-2"
             >
-              Economy Rooms:{" "}
+              Economy Rooms
             </label>
             <input
               id="numberOfEconomyRooms"
@@ -74,7 +74,7 @@ export const AvailableRoomForm = (props: IAvalableRoomFormProps) => {
             className="text-white bg-blue-700 hover:bg-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center"
             type="submit"
           >
-            Calculate Earnings
+            Calculate Revenue
           </button>
         </div>
       </form>
